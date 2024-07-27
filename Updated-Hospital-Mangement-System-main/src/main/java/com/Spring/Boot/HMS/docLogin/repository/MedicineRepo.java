36a058fcc63ba31c75ddafde18a3eba6f904e887
Neeraj singh Bhandari <92501172+Neeraj-Bhandari44@users.noreply.github.com>
2024-07-27 19:51:39 +0530

    Add files via upload

---
+++ Updated-Hospital-Mangement-System-main/src/main/java/com/Spring/Boot/HMS/docLogin/repository/MedicineRepo.java
@@ -0,0 +1,11 @@
+package com.Spring.Boot.HMS.docLogin.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Spring.Boot.HMS.docLogin.entity.Medicine;
+
+@Repository
+public interface MedicineRepo  extends JpaRepository<Medicine, Long>{
+
+}
